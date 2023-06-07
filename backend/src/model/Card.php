76289cd8616e5e9f4bf6c195b7135d63eb18b34f
[Minh Duc Nguyen Cao] <[duc-cao@cybozu.vn]>
2023-06-07 16:42:33 +0700

    feat: init board and card model

---
+++ backend/src/model/Card.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace D002834\Backend\model;
+
+class Card
+{
+    private int $id;
+    private string $title;
+    private string $description;
+    private string $created_at;
+    private string $updated_at;
+
+    public function __construct(int $id, string $title, string $description, string $created_at, string $updated_at)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->created_at = $created_at;
+        $this->updated_at = $updated_at;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return htmlspecialchars($this->title);
+    }
+
+    public function getDescription(): string
+    {
+        return htmlspecialchars($this->description);
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
+
+}
