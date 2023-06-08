a0f4b8a63547192869bfad45d519a7fb1fe0d4a8
[Minh Duc Nguyen Cao] <[duc-cao@cybozu.vn]>
2023-06-08 11:01:19 +0700

    refactor: interceptor -> class

---
+++ backend/src/interface/middleware/Interceptor.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace D002834\Backend\interface\middleware;
+
+class Interceptor
+{
+    public function __construct()
+    {
+        // Overwrite header to avoid CORS error when integration testing
+        header("Content-Type: application/json");
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Access, Content-Type, Authorization");
+        // Setting content security policy to protect against XSS
+        header("Content-Security-Policy: default-src 'self'; script-src 'self' http://localhost:3000; connect-src 'self' http://localhost:8000;");
+    }
+
+    public function handleRequest(): void
+    {
+        define("REQUEST_METHOD", $_SERVER["REQUEST_METHOD"]);
+        $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $script_path = dirname($_SERVER['SCRIPT_NAME']);
+
+        // Remove script path from request URI, if necessary
+        if (str_starts_with($request_uri, $script_path)) {
+            $request_uri = substr($request_uri, strlen($script_path));
+        }
+
+        $request_uri = explode('/', trim($request_uri, '/'));
+        define("REQUEST_URI", $request_uri);
+
+        $resource = array_shift($request_uri);
+        define("RESOURCE", $resource);
+
+        include __DIR__ . "/router.php";
+
+        // Use IIFE to avoid polluting global namespace
+        (function (string $resource, string $request_method): void {
+            $request_body = json_decode(file_get_contents('php://input'), true);
+            switch ($resource) {
+                case 'login':
+                    login_routing($request_method, $request_body);
+                    break;
+                case 'register':
+                    register_routing($request_method, $request_body);
+                    break;
+                case 'users':
+                    user_routing($request_method, $request_body);
+                    break;
+                default:
+                    http_response_code(404);
+                    echo json_encode(["message" => "Resource not found"]);
+            }
+        })(RESOURCE, REQUEST_METHOD);
+    }
+}
+
+// Usage:
+$interceptor = new Interceptor();
+$interceptor->handleRequest();
