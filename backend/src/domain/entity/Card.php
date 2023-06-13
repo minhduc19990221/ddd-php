7fea5ae85d0b1b8a00cd360860a874250c9a731b
[Minh Duc Nguyen Cao] <[duc-cao@cybozu.vn]>
2023-06-13 16:33:32 +0700

    fix: add board_id when return card entity

--- backend/src/domain/entity/Card.php
+++ backend/src/domain/entity/Card.php
@@ -7,12 +7,15 @@ class Card
     private int $id;
     private string $title;
     private int $index_board;
+    private int $board_id;
 
-    public function __construct(int $id, string $title, int $index_board)
+
+    public function __construct(int $id, string $title, int $index_board, int $board_id)
     {
         $this->id = $id;
         $this->title = $title;
         $this->index_board = $index_board;
+        $this->board_id = $board_id;
     }
 
     public function getId(): int
@@ -35,7 +38,8 @@ class Card
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'index_board' => $this->index_board
+            'index_board' => $this->index_board,
+            'board_id' => $this->board_id
         ];
     }
 }
